b4c1105acbd1c70b983e21566e892905ad5506d8
ErwinFeld <erwinmailkonto@gmail.com>
2021-12-03 09:47:09 +0100

    added regenerator-runtime/runtime to able to use async await

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
+/* able to use async/await in parcel reason babel polyfill */
+import 'regenerator-runtime/runtime'
+
+import { fetchContent } from "./fetchContent";
+
+
+import fetchNewsData from './fetchNewsData.json'
+
+// console.log(fetchNewsData)
+
+const {articles} = fetchNewsData;
+
+const singleArticle = articles[3].content;
+// fetchContent()
+
+/* for i of articles
+    single articel
+
+    display stuff in card
+
+    add property sentiment
+
+    function add on return
+
+    toxic
+    neutral
+    positive
+
+*/
+
+
+console.log(singleArticle)
 // import * as bootstrap from 'bootstrap'
 
-import { bigdik } from './testfunction';
+// import { bigdik } from './testfunction';
 
-bigdik()
+// bigdik()
 
 var sentimentAnalysis = require('sentiment');
 
 var sentiment = new sentimentAnalysis();
-var result = sentiment.analyze('Cats are stupid.');
+var result = sentiment.analyze(singleArticle);
 console.log(result);    // Score: -2, Comparative: -0.666
 
 
